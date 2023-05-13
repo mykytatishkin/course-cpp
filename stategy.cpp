bb95ae9de76fb62821c8f0ece3441224478397c8
Mykyta Tishkin <87891550+mykytatishkin@users.noreply.github.com>
2023-05-13 13:23:42 +0300

    init

---
+++ stategy.cpp
@@ -0,0 +1,70 @@
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+
+class Reader {  
+public:
+    virtual ~Reader() {}
+    virtual void parse(string& url) = 0;
+};
+
+class ResourceReader {
+private:
+    Reader* reader;
+public:
+    ResourceReader(Reader* r) : reader(r) {}
+    ~ResourceReader() {delete reader; }
+
+    void setStrategy(Reader* r){
+    if (reader != nullptr) delete reader;
+    reader = r;
+    }
+
+    void read(string& url){
+    reader->parse(url);
+    }
+};
+
+class NewsSiteReader : public Reader {
+public:
+    void parse(string& url) override {
+        cout << "Parsing from site" << url << endl;
+    } 
+};
+
+class SocialNetworkReader : public Reader {
+public:
+    void parse(string& url) override {
+        cout << "Parsing from social network" << url << endl;
+    } 
+};
+
+class TelegramChannelReader : public Reader {
+public:
+    void parse(string& url) override {
+        cout << "Parsing from telegram" << url << endl;
+    }
+};
+
+
+int main()
+{
+    ResourceReader* resourceReader = new ResourceReader(new NewsSiteReader());
+    vector<string> v = vector<string>();
+
+    string url = "https://news.com";
+    resourceReader->read(url);
+
+
+    url = "https://facebook.com";
+    resourceReader->setStrategy(new SocialNetworkReader());
+
+    url = "@news_channel_tg";
+    resourceReader->setStrategy(new TelegramChannelReader());
+    resourceReader->read(url);
+
+
+    
+    return 0;
+}
